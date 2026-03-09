68f2fa122816cbe0c2e71a44279e11cae12ad032
HeadphoneTaco <michael.hardwicke92@gmail.com>
2026-03-09 14:11:10 -0600

    Checkpoint trigger

---
+++ UnityProjects/m-h-g1402-ass-2/Assets/Scripts/CheckpointTrigger.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+// Attach to a trigger volume next to a SpawnPoint.
+// When the player walks through, updates the active spawn point.
+[RequireComponent(typeof(Collider))]
+public class CheckpointTrigger : MonoBehaviour
+{
+    [SerializeField] private SpawnPoint spawnPoint;
+
+    private void Start()
+    {
+        // Make sure collider is a trigger
+        GetComponent<Collider>().isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            RespawnManager.Instance.SetSpawnPoint(spawnPoint);
+        }
+    }
+}
