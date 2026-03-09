5fc8edde03817e237854fbde74d15ec293d18298
HeadphoneTaco <michael.hardwicke92@gmail.com>
2026-03-09 14:11:39 -0600

    Respawn manager

---
+++ UnityProjects/m-h-g1402-ass-2/Assets/Scripts/RespawnManager.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using UnityEngine;
+using YeetThePlayer;
+
+public class RespawnManager : MonoBehaviour
+{
+    public static RespawnManager Instance;
+
+    [SerializeField] private Health playerHealth;
+    [SerializeField] private PlayerController playerController;
+    [SerializeField] private Animator playerAnimator;
+    [SerializeField] private SpawnPoint activeSpawnPoint;
+    [SerializeField] private GameObject ghostPrefab;
+
+    [Space(10)]
+    [SerializeField] private float deathDelay = 2f;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
+    private void OnEnable()
+    {
+        playerHealth.OnDeath += HandleDeath;
+    }
+
+    private void OnDisable()
+    {
+        playerHealth.OnDeath -= HandleDeath;
+    }
+
+    private void HandleDeath()
+    {
+        StartCoroutine(DeathSequence());
+    }
+
+    private IEnumerator DeathSequence()
+    {
+        // Stop player movement
+        playerController.enabled = false;
+
+        // Play death animation if available
+        if (playerAnimator != null)
+        {
+            playerAnimator.SetTrigger("Death");
+        }
+
+        // Spawn ghost VFX at player position
+        if (ghostPrefab != null)
+        {
+            Instantiate(ghostPrefab, playerController.transform.position, Quaternion.identity);
+        }
+
+        // Wait for death delay
+        yield return new WaitForSeconds(deathDelay);
+
+        // Teleport to spawn point
+        playerController.transform.position = activeSpawnPoint.transform.position;
+
+        // Reset health
+        playerHealth.ResetHealth();
+
+        // Re-enable player
+        playerController.enabled = true;
+    }
+
+    // Called by CheckpointTrigger when player hits a checkpoint
+    public void SetSpawnPoint(SpawnPoint newSpawnPoint)
+    {
+        activeSpawnPoint = newSpawnPoint;
+        Debug.Log($"Checkpoint set: {newSpawnPoint.gameObject.name}");
+    }
+}
