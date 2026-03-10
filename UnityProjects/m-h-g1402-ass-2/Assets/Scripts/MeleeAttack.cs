6acfeda9d1a11a226deea74b3afa0f3d14f717a5
HeadphoneTaco <michael.hardwicke92@gmail.com>
2026-03-10 10:35:21 -0600

    Melee attack wip

--- UnityProjects/m-h-g1402-ass-2/Assets/Scripts/MeleeAttack.cs
+++ UnityProjects/m-h-g1402-ass-2/Assets/Scripts/MeleeAttack.cs
@@ -3,16 +3,45 @@ using UnityEngine.InputSystem;
 
 public class MeleeAttack : MonoBehaviour
 {
+    [SerializeField] private InputAction meleeInput;
     [SerializeField] private Animator animator;
     [SerializeField] private MeleeHitbox hitbox;
 
     private static readonly int MeleeParam = Animator.StringToHash("Melee");
+    private static readonly int MeleeStateHash = Animator.StringToHash("Melee_Attack");
 
-    private void OnMeleeAttack(InputValue value)
+    private bool _isAttacking;
+
+    private void OnEnable()
     {
-        if (value.isPressed)
+        meleeInput.Enable();
+        meleeInput.performed += OnMeleePerformed;
+    }
+
+    private void OnDisable()
+    {
+        meleeInput.performed -= OnMeleePerformed;
+    }
+
+    private void OnMeleePerformed(InputAction.CallbackContext context)
+    {
+        animator.SetTrigger(MeleeParam);
+    }
+
+    private void Update()
+    {
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        bool inMeleeState = stateInfo.shortNameHash == MeleeStateHash;
+
+        if (inMeleeState && !_isAttacking)
         {
-            animator.SetTrigger(MeleeParam);
+            _isAttacking = true;
+            hitbox.EnableHitbox();
+        }
+        else if (!inMeleeState && _isAttacking)
+        {
+            _isAttacking = false;
+            hitbox.DisableHitbox();
         }
     }
 }
