01792942cce56cd0cf980a3b0656dc73124a6b89
HeadphoneTaco <michael.hardwicke92@gmail.com>
2026-03-18 18:48:43 -0600

    CrossHair

---
+++ UnityProjects/m-h-g1402-ass-2/Assets/Scripts/CrossHair.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class CrossHair : MonoBehaviour
+{
+       [SerializeField] private PlayerController player;
+       [SerializeField] private Canvas crossHairCanvas;
+
+    private void OnEnable()
+    {
+           player.OnStateUpdated += StateUpdate;
+    }
+    
+    void OnDisable()
+    {
+        player.OnStateUpdated -= StateUpdate;
+    }
+
+    private void Start()
+    {
+        crossHairCanvas.enabled = false;
+    }
+
+    void StateUpdate(PlayerState state)
+    {
+        crossHairCanvas.enabled = state == PlayerState.AIM;
+    }
+}
