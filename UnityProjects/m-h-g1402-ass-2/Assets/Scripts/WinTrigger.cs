193f35f0742f2273f588a72a8ef24da25bb43b83
HeadphoneTaco <michael.hardwicke92@gmail.com>
2026-03-19 14:02:50 -0600

    Win and Lose scripts

---
+++ UnityProjects/m-h-g1402-ass-2/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class WinTrigger : MonoBehaviour
+{
+    private void Start()
+    {
+        GetComponent<Collider>().isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GameManager.Instance.TriggerWin();
+        }
+    }
+}
