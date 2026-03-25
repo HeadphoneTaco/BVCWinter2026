08158404a3e33d48528cfdaf63d16927da596ef8
HeadphoneTaco <michael.hardwicke92@gmail.com>
2026-03-25 10:12:57 -0600

    Player Animator/Controller/Interactor Comments

--- UnityProjects/m-h-g1402-ass-2/Assets/Scripts/PlayerAnimator.cs
+++ UnityProjects/m-h-g1402-ass-2/Assets/Scripts/PlayerAnimator.cs
@@ -1,34 +1,57 @@
 using UnityEngine;
 
+/// <summary>
+/// Synchronizes player movement state with animator parameters and triggers jump animations.
+/// </summary>
 public class PlayerAnimator : MonoBehaviour
 {
+    /// <summary>
+    /// Reference to the player controller that provides movement and event data.
+    /// </summary>
     [SerializeField] private PlayerController playerController;
+
+    /// <summary>
+    /// Animator component used to set animation parameters and triggers.
+    /// </summary>
     [SerializeField] private Animator anim;
 
-    Vector3 _playerVelocity;
+    /// <summary>
+    /// Cached player velocity used to drive blend values in the animator.
+    /// </summary>
+    private Vector3 _playerVelocity;
 
-    void Update()
+    /// <summary>
+    /// Updates animator parameters every frame based on the player's current movement state.
+    /// </summary>
+    private void Update()
     {
         anim.SetBool("IsGrounded", playerController.IsGrounded());
 
         _playerVelocity = playerController.GetPlayerVelocity();
         _playerVelocity.y = 0;
-        
+
         anim.SetFloat("Velocity", _playerVelocity.sqrMagnitude);
     }
 
-    //Subscribe to Events
-    void OnEnable()
+    /// <summary>
+    /// Subscribes to player events when this component becomes enabled.
+    /// </summary>
+    private void OnEnable()
     {
         playerController.OnJumpEvent += OnJump;
     }
-    
-    //Unsubscribe to Events
-    void OnDisable()
+
+    /// <summary>
+    /// Unsubscribes from player events when this component becomes disabled.
+    /// </summary>
+    private void OnDisable()
     {
         playerController.OnJumpEvent -= OnJump;
     }
 
+    /// <summary>
+    /// Handles the player jump event by triggering the jump animation.
+    /// </summary>
     private void OnJump()
     {
         anim.SetTrigger("Jump");
