32f901fa6b7fdc1d2029652e624d40fb72b06bdf
HeadphoneTaco <michael.hardwicke92@gmail.com>
2026-03-25 11:08:36 -0600

    Health/CoinPickups, Health, Collector Comments

--- UnityProjects/m-h-g1402-ass-2/Assets/Scripts/CoinPickup.cs
+++ UnityProjects/m-h-g1402-ass-2/Assets/Scripts/CoinPickup.cs
@@ -3,13 +3,21 @@ using Managers;
 using UI;
 using UnityEngine;
 
+/// <summary>
+/// Represents a collectible coin pickup in the scene.
+/// When collected, it updates the coin UI, plays a collect sound, and removes itself.
+/// </summary>
 public class CoinPickup : MonoBehaviour, ICollectable
 {
+    /// <summary>
+    /// Handles coin collection logic for the colliding collector object.
+    /// </summary>
+    /// <param name="collector">
+    /// The GameObject that collected this coin. Included to satisfy the <see cref="ICollectable"/> contract.
+    /// </param>
     public void OnCollect(GameObject collector)
     {
-        FindObjectOfType<CoinUI>()?.AddCoin();
-        Debug.Log("Coin Collected");
-        //Make sound
+        FindAnyObjectByType<CoinUI>()?.AddCoin();
         AudioManager.Instance?.PlayCollect();
         Destroy(gameObject);
     }
--- UnityProjects/m-h-g1402-ass-2/Assets/Scripts/Collector.cs
+++ UnityProjects/m-h-g1402-ass-2/Assets/Scripts/Collector.cs
@@ -1,15 +1,22 @@
 using Interfaces;
 using UnityEngine;
 
+/// <summary>
+/// Detects trigger collisions and forwards collection events to objects
+/// that implement the <see cref="ICollectable"/> interface.
+/// </summary>
 public class Collector : MonoBehaviour
 {
-    void OnTriggerEnter(Collider other)
+    /// <summary>
+    /// Called by Unity when another collider enters this trigger collider.
+    /// If the entering object is collectable, invokes its collect handler
+    /// and passes this collector's <see cref="GameObject"/> as the collector.
+    /// </summary>
+    /// <param name="other">The collider that entered this trigger.</param>
+    private void OnTriggerEnter(Collider other)
     {
-        ICollectable otherCollectable = other.GetComponent<ICollectable>();
+        var otherCollectable = other.GetComponent<ICollectable>();
 
-        if (otherCollectable != null)
-        {
-            otherCollectable.OnCollect(gameObject);
-        }
+        if (otherCollectable != null) otherCollectable.OnCollect(gameObject);
     }
 }
