15f0a63a8e24721cf66f842150a53c5370e58dd9
HeadphoneTaco <michael.hardwicke92@gmail.com>
2026-03-25 13:47:25 -0600

    Start Menu Comments, Regenerate VS Nodes

--- UnityProjects/m-h-g1402-ass-2/Assets/Scripts/UI/StartMenu.cs
+++ UnityProjects/m-h-g1402-ass-2/Assets/Scripts/UI/StartMenu.cs
@@ -1,29 +1,51 @@
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class StartMenu : MonoBehaviour
+namespace UI
 {
-    [SerializeField] private Button playButton;
-    [SerializeField] private Button quitButton;
-
-    private void Start()
+    /// <summary>
+    /// Controls the start menu UI by wiring button events for starting or quitting the game.
+    /// </summary>
+    public class StartMenu : MonoBehaviour
     {
-        playButton.onClick.AddListener(OnPlayClicked);
-        quitButton.onClick.AddListener(OnQuitClicked);
-    }
-
-    private void OnPlayClicked()
-    {
-        SceneManager.LoadScene("Main");
-    }
-
-    private void OnQuitClicked()
-    {
-        Application.Quit();
-        // Stops play mode in editor since Application.Quit() won't work there
+        /// <summary>
+        /// Button that starts the main game scene.
+        /// </summary>
+        [SerializeField] private Button playButton;
+    
+        /// <summary>
+        /// Button that exits the application.
+        /// </summary>
+        [SerializeField] private Button quitButton;
+    
+        /// <summary>
+        /// Registers click listeners for the start menu buttons when the component initializes.
+        /// </summary>
+        private void Start()
+        {
+            playButton.onClick.AddListener(OnPlayClicked);
+            quitButton.onClick.AddListener(OnQuitClicked);
+        }
+    
+        /// <summary>
+        /// Loads the main gameplay scene.
+        /// </summary>
+        private void OnPlayClicked()
+        {
+            SceneManager.LoadScene("Main");
+        }
+    
+        /// <summary>
+        /// Quits the application. In the Unity Editor, this also stops Play mode.
+        /// </summary>
+        private void OnQuitClicked()
+        {
+            Application.Quit();
+            // Stops play mode in editor since Application.Quit() won't work there
 #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
+            UnityEditor.EditorApplication.isPlaying = false;
 #endif
+        }
     }
 }
